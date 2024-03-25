83ed95cf5e9dc052065bcf51a1d980513c421a6a
Aman Kutlaria <rajaman.rajput09@gmail.com>
2024-03-25 21:21:30 +0530

    DSA

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,21 @@
-function calculator() {}
+function calculator(num1,num2,operator) {
+    let result;
+    if (operator === '+' ){
+        result = num1 + num2;
+    }
+    else if(operator === '-'){
+        result = num1 - num2;
+    }
+    else if(operator === '*'){
+        result = num1*num2;
+    }
+    else if(operator === '/'){
+        result = num1/num2;
+    }
+    else 
+    console.log("Invalid Operator");
+
+    return result;
+}
 
 module.exports = calculator;
